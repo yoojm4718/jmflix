0cd429d3520d9eb3ee632542c86fe5dc86ce0dfa
yoojm4718 <yoojm4718@outlook.com>
2021-09-10 11:38:05 +0900

    Styling Components Using styled-components

---
+++ src/Components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const SHeader = styled.header``;
+
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
+
+const Header = () => (
+  <SHeader>
+    <List>
+      <Item>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </SHeader>
+);
+
+export default Header;
