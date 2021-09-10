6180f8eacf5172558fde0bf1dafa5d2d4566aed5
yoojm4718 <yoojm4718@outlook.com>
2021-09-10 11:59:21 +0900

    GlobalStyle Apply with styled-reset, Basic Styles Applied

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import Router from "Components/Router";
+import GlobalStyles from "./GlobalStyles";
 
 class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     );
   }
